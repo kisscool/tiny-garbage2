2768e1c44b395f3fce71820ae763bd3e7775a2e9
KissCool <bayle.philippe@gmail.com>
2010-11-15 17:13:16 +0100

    the tcp ping timeout is 2 seconds again, because of issues with the threadpool

--- scripts/crawler.rb
+++ scripts/crawler.rb
@@ -82,9 +82,9 @@ def expand_network(networks)
   expanded_ip_list.flatten
 end
 
-# check if an host is alive on the TCP port 21 (3 second timeout)
+# check if an host is alive on the TCP port 21 (2 second timeout)
 def ping_tcp(ip)
-  Ping.pingecho(ip, 3, 21)
+  Ping.pingecho(ip, 2, 21)
 end
 
 # check if we can make an FTP connexion with an host
